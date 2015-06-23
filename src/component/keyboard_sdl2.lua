2fc29e6f8d08daa3c955998486221dbbee88a0bb
gamax92 <gamax92@aol.com>
2015-06-23 00:11:14 -0600

    Keyboard fixes
    All non mapped codes will default to zero
    Add in a quick hack to correct the codepoints for tab and enter, which
    SDL doesn't generate a SDL_TextInputEvent for

--- src/component/keyboard_sdl2.lua
+++ src/component/keyboard_sdl2.lua
@@ -9,19 +9,29 @@ local keys = require("support.sdl_to_lwjgl")
 
 local code2char = {}
 
+local function setLatest(char)
+	kbdcodes[#kbdcodes].char = char
+	code2char[kbdcodes[#kbdcodes].code] = char
+end
+
 function elsa.textinput(event)
 	local textevent = ffi.cast("SDL_TextInputEvent", event)
 	local text = ffi.string(textevent.text)
 	cprint("textinput",text)
-	kbdcodes[#kbdcodes].char = lua_utf8.byte(text)
-	code2char[kbdcodes[#kbdcodes].code] = kbdcodes[#kbdcodes].char
+	setLatest(lua_utf8.byte(text))
 end
 
 function elsa.keydown(event)
 	local keyevent = ffi.cast("SDL_KeyboardEvent", event)
 	local key = keyevent.keysym.scancode
 	cprint("keydown",keys[key])
-	table.insert(kbdcodes,{type="key_down",addr=address,code=keys[key]})
+	table.insert(kbdcodes,{type="key_down",addr=address,code=keys[key] or 0})
+	-- TODO: Lovely SDL Hacks
+	if keys[key] == 15 then
+		setLatest(9)
+	elseif keys[key] == 28 or keys[key] == 156 then
+		setLatest(13)
+	end
 	if keys[key] == 210 then
 		if SDL.hasClipboardText() > 0 then
 			table.insert(machine.signals,{"clipboard",address,ffi.string(SDL.getClipboardText())})
