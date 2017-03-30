a951c136c27cf8c06d6aded0387b2f1bc1f13c1c
gamax92 <gamax92@aol.com>
2017-03-30 02:12:45 -0600

    Update unicode api for JNLua changes

--- src/apis/unicode.lua
+++ src/apis/unicode.lua
@@ -2,10 +2,6 @@ local env = ...
 
 local utf8 = require("lua-utf8")
 
-local function cln(str)
-	return str:gsub("%z.*","") .. ""
-end
-
 env.unicode = setmetatable({},{
 	__index = function(_,k)
 		cprint("Missing environment access", "env.unicode." .. k)
@@ -19,32 +15,28 @@ function env.unicode.char(...)
 		checkArg(i,args[i],"number")
 		args[i] = args[i]%0x10000
 	end
-	return utf8.char(table.unpack(args)):gsub("%z","\xC0\x80") .. ""
+	return utf8.char(table.unpack(args))
 end
 function env.unicode.lower(str)
 	cprint("unicode.lower", str)
 	if type(str) == "number" then str = tostring(str) end
 	checkArg(1,str,"string")
-	str=cln(str)
 	return utf8.lower(str)
 end
 function env.unicode.upper(str)
 	cprint("unicode.upper", str)
 	if type(str) == "number" then str = tostring(str) end
 	checkArg(1,str,"string")
-	str=cln(str)
 	return utf8.upper(str)
 end
 function env.unicode.len(str)
 	cprint("unicode.len", str)
 	checkArg(1,str,"string")
-	str=cln(str)
 	return utf8.len(str)
 end
 function env.unicode.reverse(str)
 	cprint("unicode.reverse", str)
 	checkArg(1,str,"string")
-	str=cln(str)
 	return utf8.reverse(str)
 end
 function env.unicode.sub(str, i, j)
@@ -53,13 +45,11 @@ function env.unicode.sub(str, i, j)
 	checkArg(2,i,"number")
 	if j == nil then j = -1 end
 	checkArg(3,j,"number")
-	str=cln(str)
 	return utf8.sub(str,i,j)
 end
 function env.unicode.isWide(str)
 	cprint("unicode.isWide", str)
 	checkArg(1,str,"string")
-	str=cln(str)
 	if #str == 0 then
 		error("String index out of range: 0", 0)
 	end
@@ -69,7 +59,6 @@ end
 function env.unicode.charWidth(str)
 	cprint("unicode.charWidth", str)
 	checkArg(1,str,"string")
-	str=cln(str)
 	if #str == 0 then
 		error("String index out of range: 0", 0)
 	end
@@ -79,7 +68,6 @@ end
 function env.unicode.wlen(str)
 	cprint("unicode.wlen", str)
 	checkArg(1,str,"string")
-	str=cln(str)
 	local length = 0
 	for _,c in utf8.next, str do
 		length = length + getCharWidth(c)
@@ -90,7 +78,6 @@ function env.unicode.wtrunc(str, count)
 	cprint("unicode.wtrunc", str, count)
 	checkArg(1,str,"string")
 	checkArg(2,count,"number")
-	str=cln(str)
 	if count == math.huge then
 		count = 0
 	end
